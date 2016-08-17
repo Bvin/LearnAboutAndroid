6c6f3ff9080cd37658b3702283b8993ffa68604e
bvin <bvin@live.cn>
2016-08-17 19:17:14 +0800

    first commit

---
+++ checkboxon4.x/src/main/java/cn/bvin/lib/checkboxon4x/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.bvin.lib.checkboxon4x;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
