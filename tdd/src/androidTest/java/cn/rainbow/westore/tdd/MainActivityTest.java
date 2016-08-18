63d2697261f6b72a1abf310fdefae419bbc671ff
黄绍斌 <huangshaobin@rainbowcn.com>
2016-08-18 13:35:08 +0800

    单元测试

---
+++ tdd/src/androidTest/java/cn/rainbow/westore/tdd/MainActivityTest.java
@@ -0,0 +1,19 @@
+package cn.rainbow.westore.tdd;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bvin on 2016/8/18.
+ */
+public class MainActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+
+}
