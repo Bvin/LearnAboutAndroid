69f2a188358ad8a03e097d8b241c1428b223bb00
黄绍斌 <huangshaobin@rainbowcn.com>
2016-10-31 17:03:20 +0800

    ViewGroup.addView(view,index)有效

--- bottomsheet/src/main/java/cn/bvin/app/bottomsheet/ContextActivity.java
+++ bottomsheet/src/main/java/cn/bvin/app/bottomsheet/ContextActivity.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,6 +32,14 @@ public class ContextActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_context);
+        ViewGroup frame = (ViewGroup) findViewById(R.id.activity_context);
+        TextView tv = new TextView(ContextActivity.this);
+        CoordinatorLayout.LayoutParams lp = new CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.WRAP_CONTENT,CoordinatorLayout.LayoutParams.MATCH_PARENT);
+        tv.setLayoutParams(lp);
+        tv.setText("Ggh啊护身复活ipaPI");
+        tv.setTextColor(ResourcesFinder.getColor(getResources(),R.color.colorPrimaryDark));
+        tv.setBackgroundColor(ResourcesFinder.getColor(getResources(),R.color.colorAccent));
+        frame.addView(tv,0);
         List<String> data = new ArrayList<>();
         data.add("1");
         data.add("1");
