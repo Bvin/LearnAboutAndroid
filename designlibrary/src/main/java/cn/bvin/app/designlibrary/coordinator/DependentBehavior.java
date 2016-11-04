6fed36182c15a5a1aa5bffda90b218cb2a56f5cc
bvin <bvin@live.cn>
2016-11-04 11:06:03 +0800

    DependentBehavior实践

---
+++ designlibrary/src/main/java/cn/bvin/app/designlibrary/coordinator/DependentBehavior.java
@@ -0,0 +1,38 @@
+package cn.bvin.app.designlibrary.coordinator;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bvin on 2016/11/4.
+ */
+
+public class DependentBehavior extends CoordinatorLayout.Behavior<View> {
+
+    /**
+     * Default constructor for inflating Behaviors from layout. The Behavior will have
+     * the opportunity to parse specially defined layout parameters. These parameters will
+     * appear on the child view tag.
+     *
+     * @param context
+     * @param attrs
+     */
+    public DependentBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        ViewCompat.setTranslationY(child,Math.abs(dependency.getTop()));
+        return true;
+    }
+}
