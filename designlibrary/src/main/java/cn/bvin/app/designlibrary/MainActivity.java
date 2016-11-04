d643d06434f02b39b47e5a60e7fb620b35f42e94
黄绍斌 <huangshaobin@rainbowcn.com>
2016-11-04 09:57:56 +0800

    CoordinatorLayout

--- designlibrary/src/main/java/cn/bvin/app/designlibrary/MainActivity.java
+++ designlibrary/src/main/java/cn/bvin/app/designlibrary/MainActivity.java
@@ -2,9 +2,11 @@ package cn.bvin.app.designlibrary;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 return false;
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CoordinatorActivity.start(MainActivity.this);
             }
         });
     }
