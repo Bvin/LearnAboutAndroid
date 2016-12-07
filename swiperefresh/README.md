d851e7c0cce3effb6f03a0976f3ff0e3cdd66d44
bvin <bvin@live.cn>
2016-12-07 14:47:34 +0800

    方案可行

--- swiperefresh/README.md
+++ swiperefresh/README.md
@@ -1,8 +1,24 @@
 FreeLine环境：
+
 1. 安装Python 2.7，并加入环境变量
 2. CompileSdk<=23，TargetSdk<=23, supportLibrary<=23
 3. com.android.tools.build:gradle:2.1.2
 4. 安装FreeLine插件
 
 SwipeRefresh：
-1.SwipeRefreshLayout宽高是match_parent，手动设置wrap_content无效
+
+1. SwipeRefreshLayout宽高是match_parent，手动设置wrap_content无效
+
+CoordinateLayout:
+
+1. CoordinateLayout可以作为SwipeRefreshLayout的子View
+
+
+AppBarLayout： 
+AppBarLayout是实现了很多material designs 设计风格应用栏（即滑动手势）功能的一种竖向的线性布局。
+
+1. AppBarLayout的子View应该指定layout_scrollFlags
+2. AppBarLayout必须作为CoordinateLayout的直接子View，否则将很多特性将不支持
+3. AppBarLayout需要滑动的View绑定AppBarLayout.ScrollingViewBehavior 来关联滑动行为
+
+4. AppBarLayout的大部分作用都是处理Scroll，可以认为是在有scroll的情况才能体现它的作用
--- swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
+++ swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
@@ -1,16 +1,17 @@
 package cn.bvin.app.swiperefresh;
 
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        setContentView(R.layout.main_container);
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main);
+        swipeRefreshLayout.setEnabled(false);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
