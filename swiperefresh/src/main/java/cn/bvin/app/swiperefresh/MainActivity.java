cb58e516059a57d91ac3135dc52c5a210627e559
bvin <bvin@live.cn>
2016-12-07 14:53:52 +0800

    解决下拉刷新和滑动view冲突

--- swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
+++ swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
@@ -1,6 +1,7 @@
 package cn.bvin.app.swiperefresh;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,11 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_container);
         final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main);
-        swipeRefreshLayout.setEnabled(false);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+                swipeRefreshLayout.setEnabled(i == 0);
             }
         });
     }
