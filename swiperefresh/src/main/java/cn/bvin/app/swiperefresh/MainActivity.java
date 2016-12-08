d807e62016add554ff850e772899c49c73e4bcb8
bvin <bvin@live.cn>
2016-12-08 15:23:31 +0800

    手动刷新

--- swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
+++ swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
 
     private OriginSwipeRefreshLayout swipeRefreshLayout;
@@ -17,12 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_container);
         swipeRefreshLayout = (OriginSwipeRefreshLayout) findViewById(R.id.activity_main);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        });
+        swipeRefreshLayout.setOnRefreshListener(this);
 
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
@@ -31,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
                 swipeRefreshLayout.setEnabled(i == 0);
             }
         });
+    }
+
+    @Override
+    public void onRefresh() {
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.refresh){
             swipeRefreshLayout.setRefreshing(true);
+            onRefresh();
             return true;
         }
         return super.onOptionsItemSelected(item);
