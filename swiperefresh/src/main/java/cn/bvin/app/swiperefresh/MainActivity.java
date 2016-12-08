55197edacc4e5ddc105845bd8f4c6c07351eb56e
bvin <bvin@live.cn>
2016-12-08 14:08:08 +0800

    添加自定义菜单

--- swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
+++ swiperefresh/src/main/java/cn/bvin/app/swiperefresh/MainActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private OriginSwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_container);
-        final OriginSwipeRefreshLayout swipeRefreshLayout = (OriginSwipeRefreshLayout) findViewById(R.id.activity_main);
+        swipeRefreshLayout = (OriginSwipeRefreshLayout) findViewById(R.id.activity_main);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -26,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
                 swipeRefreshLayout.setEnabled(i == 0);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.refresh){
+            swipeRefreshLayout.setRefreshing(true);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
