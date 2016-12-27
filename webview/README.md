62916d0e26e49864256039e38b94c9c9840e47c4
bvin <bvin@live.cn>
2016-12-27 15:28:02 +0800

    update

--- webview/README.md
+++ webview/README.md
@@ -1,2 +1,5 @@
 1. webView加载html乱码，需要用baseUrl
 2. 一定要webView.setWebChromeClient()，js：alert才有效
+3. webView.loadData()方法不能加载js代码，将会当作html字符串
+4. 如果loadUrl中的url以javascript:开头就会执行js代码，而不会执行到onPageFinish
+     否则每load一次就会回掉一次onPageFinish
--- webview/src/main/java/cn/bvin/app/webview/MainActivity.java
+++ webview/src/main/java/cn/bvin/app/webview/MainActivity.java
@@ -3,20 +3,25 @@ package cn.bvin.app.webview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import cn.bvin.library.common.web.JavascriptBridge;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView = (WebView) findViewById(R.id.web);
+        webView = (WebView) findViewById(R.id.web);
         String html = "<div style=\"max-width:480px;margin:0 auto;\">" +
                 "<style type=\"text/css\">p {max-width:480px;} img {width: 100%;height:auto;} input {width: 100%;height:auto;}" +
                 "</style>" +
@@ -35,8 +40,35 @@ public class MainActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 Log.d(TAG, "onPageFinished: "+url);
-                view.loadUrl("javascript:alert('onPageFinished')");
             }
         });
+        //JavascriptBridge.with(webView).call("blank");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.load){
+            load();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void load(){
+        // 如果loadUrl中的url以javascript:开头就会执行js代码，而不会执行到onPageFinish
+        // 否则每load一次就会回掉一次onPageFinish
+        webView.loadUrl("javascriptblank");
+
+        // 此时的url为data:text/html,javascript:alert(injectedObject.toString())
+        // 由此可见loadData不能加载js代码，将当作普通html字符串
+        //webView.loadData("javascript:alert(injectedObject.toString())","text/html","utf-8");
+    }
+
+
 }
