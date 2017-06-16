675fb0913043cb0ae2acfb8b87e48ce0d984ad19
bvin <bvin@live.cn>
2017-06-16 17:27:10 +0800

    修改界面

--- groundcalculator/src/main/java/cn/bvin/android/app/groundcalculator/MainActivity.java
+++ groundcalculator/src/main/java/cn/bvin/android/app/groundcalculator/MainActivity.java
@@ -1,30 +1,28 @@
 package cn.bvin.android.app.groundcalculator;
 
 import android.content.Context;
-import android.database.DataSetObserver;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner spSquareUnit;
-    private EditText etSquare;
-    private Spinner spFar;
-    private EditText etFar;
-    private Spinner spPriceUnit;
-    private EditText etPrice;
-    private TextView tvResult;
+    private Spinner mSpSquareUnit;
+    private EditText mEtSquare;
+    private Spinner mSpFar;
+    private EditText mEtFar;
+    private Spinner mSpPriceUnit;
+    private EditText mEtPrice;
+    private TextView mTvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +30,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        spSquareUnit = (Spinner) findViewById(R.id.sp_square_unit);
-        etSquare = (EditText) findViewById(R.id.et_square);
-        spFar = (Spinner) findViewById(R.id.sp_far);
-        etFar = (EditText) findViewById(R.id.et_far);
-        spPriceUnit = (Spinner) findViewById(R.id.sp_price_unit);
-        etPrice = (EditText) findViewById(R.id.et_price);
-        tvResult = (TextView) findViewById(R.id.tv_result);
+        mSpSquareUnit = (Spinner) findViewById(R.id.sp_square_unit);
+        mEtSquare = (EditText) findViewById(R.id.et_square);
+        mSpFar = (Spinner) findViewById(R.id.sp_far);
+        mEtFar = (EditText) findViewById(R.id.et_far);
+        mSpPriceUnit = (Spinner) findViewById(R.id.sp_price_unit);
+        mEtPrice = (EditText) findViewById(R.id.et_price);
+        mTvResult = (TextView) findViewById(R.id.tv_result);
 
     }
 
@@ -57,15 +55,15 @@ public class MainActivity extends AppCompatActivity {
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);//隐藏输入法
             }
-            BigDecimal groundSquare =  new BigDecimal(etSquare.getText().toString());//亩
-            BigDecimal price = new BigDecimal(etPrice.getText().toString());
+            BigDecimal groundSquare =  new BigDecimal(mEtSquare.getText().toString());//亩
+            BigDecimal price = new BigDecimal(mEtPrice.getText().toString());
             price = price.multiply(new BigDecimal(10000));//换算元
             BigDecimal totalPrice = price.multiply(groundSquare);//只要面积和单价单位一致便可直接换算
             groundSquare = groundSquare.multiply(new BigDecimal(666.6666667));//换算平方米
-            BigDecimal far = new BigDecimal(etFar.getText().toString());
+            BigDecimal far = new BigDecimal(mEtFar.getText().toString());
             BigDecimal buildingSquare = groundSquare.multiply(far);//土地面积*容积率
             BigDecimal result = totalPrice.divide(buildingSquare,BigDecimal.ROUND_HALF_EVEN);
-            tvResult.setText(result.toString()+"元/㎡");
+            mTvResult.setText(result.toString()+"元/㎡");
         }
         return true;
     }
