8b846ceb54f3daec567decc7eb0ee7acac6e7ff5
bvin <bvin@live.cn>
2017-08-18 10:48:29 +0800

    赣州市区划

--- futuremap/src/main/java/cn/bvin/android/app/gzmap/MainActivity.java
+++ futuremap/src/main/java/cn/bvin/android/app/gzmap/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.bvin.android.app.gzmap;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,6 +10,9 @@ import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.SupportMapFragment;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
+import com.amap.api.maps.model.PolylineOptions;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.district.DistrictItem;
 import com.amap.api.services.district.DistrictResult;
 import com.amap.api.services.district.DistrictSearch;
 import com.amap.api.services.district.DistrictSearchQuery;
@@ -17,17 +21,19 @@ import com.amap.api.services.district.DistrictSearchQuery;
 public class MainActivity extends AppCompatActivity {
 
 
+    private AMap mAMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-        AMap map = fragment.getMap();
+        mAMap = fragment.getMap();
         LatLng southwest = new LatLng(25.58105,114.704015);
         LatLng northeast = new LatLng(25.932933,115.089223);
         LatLngBounds lb = new LatLngBounds(southwest,northeast);
-        map.animateCamera(CameraUpdateFactory.newLatLngBounds(lb,0));
+        mAMap.animateCamera(CameraUpdateFactory.newLatLngBounds(lb,0));
 
         DistrictSearch districtSearch = new DistrictSearch(this);
         DistrictSearchQuery districtSearchQuery = new DistrictSearchQuery();
@@ -37,11 +43,66 @@ public class MainActivity extends AppCompatActivity {
         districtSearch.setQuery(districtSearchQuery);
         districtSearch.setOnDistrictSearchListener(new DistrictSearch.OnDistrictSearchListener() {
             @Override
-            public void onDistrictSearched(DistrictResult districtResult) {
-                Log.d("onDistrictSearched: ",districtResult.toString());
+            public void onDistrictSearched(final DistrictResult districtResult) {
+
+                if (districtResult == null || districtResult.getDistrict()==null) {
+                    return;
+                }
+                if(districtResult.getAMapException() != null && districtResult.getAMapException().getErrorCode() == AMapException.CODE_AMAP_SUCCESS){
+
+
+                    new Thread() {
+                        public void run() {
+                            DistrictItem self = districtResult.getDistrict().get(0);
+                            addDistrictItem(self);
+                            Log.d("onDistrictSearched: ",self.toString());
+                            for (DistrictItem item : self.getSubDistrict()) {
+                                Log.d("onDistrictSearched: ",item.toString());
+                                addDistrictItem(item);
+                            }
+                        }
+                    }.start();
+                }
             }
         });
         districtSearch.searchDistrictAsyn();
+    }
+
+    private void addDistrictItem( DistrictItem item) {
+
+        if (item == null) {
+            return;
+        }
+        String[] polyStr = item.districtBoundary();
+        if (polyStr == null || polyStr.length == 0) {
+            Log.d( "addDistrictItem: ","nothing");
+            return;
+        }
+        Log.d( "addDistrictItem: ","something");
+        for (String str : polyStr) {
+            String[] lat = str.split(";");
+            PolylineOptions polylineOption = new PolylineOptions();
+            boolean isFirst = true;
+            LatLng firstLatLng = null;
+            for (String latstr : lat) {
+                String[] lats = latstr.split(",");
+                if (isFirst) {
+                    isFirst = false;
+                    firstLatLng = new LatLng(Double
+                            .parseDouble(lats[1]), Double
+                            .parseDouble(lats[0]));
+                }
+                polylineOption.add(new LatLng(Double
+                        .parseDouble(lats[1]), Double
+                        .parseDouble(lats[0])));
+            }
+            if (firstLatLng != null) {
+                polylineOption.add(firstLatLng);
+            }
+
+            polylineOption.width(10).color(Color.BLUE);
+            mAMap.addPolyline(polylineOption);
+        }
     }
 
     @Override
